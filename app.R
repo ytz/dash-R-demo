83359781ba48ecbc386b62a5041152fb89ad848d
Ting Zhe Yan <1884616+ytz@users.noreply.github.com>
2022-03-09 11:56:30 -0800

    Added Choropleth, and fix layout

--- app.R
+++ app.R
@@ -2,11 +2,21 @@ library(dash)
 library(dashHtmlComponents)
 library(readr)
 library(plotly)
+library(ggplot2)
+library(broom)
+library(geojsonio)
+library(dplyr)
 
-df = read_csv("data/processed_trees.csv")
+# Input data
+df <- read_csv("data/processed_trees.csv")
+url_geojson <- "https://raw.githubusercontent.com/UBC-MDS/cherry_blossom_tracker/main/data/vancouver.geojson"
+#geojson <- rjson::fromJSON(file=url_geojson)
+geojson <- geojson_read(url_geojson, what = "sp")
+geojson2 <- tidy(geojson, region = "name")
 
 app = Dash$new(external_stylesheets = "https://codepen.io/chriddyp/pen/bWLwgP.css")
 
+# Big Street Map
 # https://plotly.com/r/mapbox-layers/
 fig <- df %>%
   plot_ly(
@@ -25,11 +35,46 @@ fig <- fig %>%
     )
   )
 
+# Choropleth
+# fig_cho <- plot_ly()
+# fig_cho <- fig_cho %>%
+#   add_trace(
+#     type="choropleth",
+#     geojson=geojson,
+#     locations=df$NEIGHBOURHOOD_NAME,
+#     z=df$TREE_ID,
+#     transforms = list(
+#       type = 'aggregate',
+#       groups = df$NEIGHBOURHOOD_NAME,
+#       aggregations = list(
+#         list(
+#           target = 'z', func = 'count', enabled = T
+#         )
+#       )
+#     )
+#   )
 
-app$layout(
+# aggregate
 
+geojson2 <- geojson2 %>%
+  left_join(df %>% count(NEIGHBOURHOOD_NAME),
+            by = c("id" = "NEIGHBOURHOOD_NAME"))
+
+fig_cho <- ggplot() +
+  geom_polygon(data = geojson2, 
+               aes(x = long, y = lat, group = group, fill = n)) +
+  coord_map()
+
+#dccGraph(figure=fig),
+#dccGraph(figure=fig_cho)
+
+app %>% set_layout(
+  div(
     dccGraph(figure=fig)
-  
+  ),
+  div(
+    dccGraph(figure=ggplotly(fig_cho))
+  )
 )
 
 app$run_server(debug = T)
